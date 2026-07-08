a14b5212d54cd2456ea2c6243d72f677db585519
Yakup Duzgun <yakup.ydzn77@gmail.com>
2026-07-08 13:00:26 +0300

    664.h45655

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 (function(root){
   'use strict';
-  var info = {"app": "V5.10", "version": "v510_yayin_oncesi_kontrol", "build": 510, "visibleVersion": "V5.10 •ExelanceX•", "shortVersion": "V5.10 •ExelanceX•", "name": "Mesaha İO V5.10 •ExelanceX•", "cacheName": "mesaha-app-v510-yayin-oncesi-kontrol", "builtAt": "2026-07-08T12:05:00+03:00", "notes": "", "assetVersion": "510", "latestVersion": "v510_yayin_oncesi_kontrol", "latestBuild": 510, "currentBuild": 510, "minSupportedBuild": 409, "forceUpdate": true, "updateUrl": "./temizle.html", "cleanUrl": "./guncelle.html"};
+  var info = {"app": "V5.11", "version": "v511_panel_tum_kullanicilar_edge", "build": 511, "visibleVersion": "V5.11 •ExelanceX•", "shortVersion": "V5.11 •ExelanceX•", "name": "Mesaha İO V5.11 •ExelanceX•", "cacheName": "mesaha-app-v511-panel-tum-kullanicilar-edge", "builtAt": "2026-07-08T12:35:00+03:00", "notes": "", "assetVersion": "511", "latestVersion": "v511_panel_tum_kullanicilar_edge", "latestBuild": 511, "currentBuild": 511, "minSupportedBuild": 409, "forceUpdate": true, "updateUrl": "./temizle.html", "cleanUrl": "./guncelle.html"};
   try{ Object.freeze(info); }catch(e){}
   function expose(name,value){
     try{ Object.defineProperty(root,name,{configurable:false,enumerable:true,get:function(){return value;},set:function(){}}); }
