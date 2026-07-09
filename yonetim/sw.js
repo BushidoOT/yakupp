e8772c19a74c845896f42b225ed8141071c54551
BushidoOT <yakup.ydzn77@gmail.com>
2026-07-09 20:16:54 +0300

    44.my225355.kt46255

--- yonetim/sw.js
+++ yonetim/sw.js
@@ -1,12 +1,5 @@
-const CACHE_NAME = "mesaha-yonetim-v509";
-const FILES = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting()));
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match("./index.html"))));
-});
+const CACHE_NAME="mesaha-yonetim-v510";
+const FILES=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
