863e59995da334ab9cc19e3c395b15129f8f182f
BushidoOT <yakup.ydzn77@gmail.com>
2026-07-09 17:43:23 +0300

    44.my22535

--- yonetim/sw.js
+++ yonetim/sw.js
@@ -1,6 +1,6 @@
-/* Mesaha İO Yönetim V5.30 */
-const CACHE='mesaha-yonetim-v530';
-const SHELL='./index.html?app=yonetim-v530';
+/* Mesaha İO Yönetim V5.31 */
+const CACHE='mesaha-yonetim-v531';
+const SHELL='./index.html?app=yonetim-v531';
 const ASSETS=['./',SHELL,'./manifest.json','../assets/icon-192.png','../assets/icon-512.png','../assets/mesaha_logo.png','../js/version.js','../js/mesaha-supabase-config.js','../js/mesaha-firebase.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>/^mesaha-yonetim-/.test(k)&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
